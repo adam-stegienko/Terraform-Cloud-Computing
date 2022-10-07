e3c5d773360c49c8f48f059f1eb6cd7bd9f08c3a
Adam Stegienko <adam.stegienko1@gmail.com>
2022-10-08 00:03:58 +0200

    WORKING // two modules + dynamic subnet number

--- prod.tfvars.tf
+++ prod.tfvars.tf
@@ -5,25 +5,29 @@ variable "ec2_data" {
     ami           = "ami-07a512ad10d82b9e2"
     instance_type = "t2.micro"
   }
+  description = "AWS EC2 instance(s) data"
 }
 
 variable "no_of_subnets" {
-  description = ""
+  description = "Number of subnets/instaces that are going to be created in a terraform apply job"
 }
 
 variable "init_file" {
   type    = string
   default = "./docker-init.sh"
+  description = "Initial set-up file that is running a docker container; stored in user_data.txt file inside EC2 instance"
 }
 
 variable "cidr_block" {
   type    = list(string)
   default = ["10.10.0.0/28", "10.10.0.192/28", ]
+  description = "CIDR blocks for subnets creation"
 }
 
 variable "availability_zone" {
   type    = list(string)
   default = ["eu-central-1a", "eu-central-1b"]
+  description = "Availability zones for subnets creation"
 }
 
 variable "adam_tags" {
@@ -31,8 +35,9 @@ variable "adam_tags" {
   default = {
     bootcamp   = "poland1"
     created_by = "Adam Stegienko"
-    Name       = "ADAM"
+    Name       = "Adam Stegienko_PROD"
   }
+  description = "Default tags for production workspace"
 }
 
 variable "health_check" {
@@ -43,6 +48,7 @@ variable "health_check" {
     timeout  = 5
     interval = 10
   }
+  description = "Health check configuration"
 }
 
 variable "alb_target" {
@@ -52,6 +58,7 @@ variable "alb_target" {
     port     = 80
     protocol = "HTTP"
   }
+  description = "Application Load Balancer's target group configuration"
 }
 
 variable "alb_balancer" {
@@ -62,6 +69,7 @@ variable "alb_balancer" {
     load_balancer_type = "application"
     ip_address_type    = "ipv4"
   }
+  description = "Application Load Balancer's configuration"
 }
 
 variable "alb_listener" {
@@ -70,18 +78,20 @@ variable "alb_listener" {
     port     = 80
     protocol = "HTTP"
   }
+  description = "Application Load Balancer listener's configuration"
 }
 
 variable "lb_listener_default_action" {
   type    = string
   default = "forward"
+  description = "Load Balancer's listener default action configuration"
 }
 
 variable "lb_security_group" {
   type = map(any)
   default = {
     name        = "adam-server-sg"
-    description = "Security Group for Adam Server"
+    description = "Security Group for Load Balancer"
   }
 }
 
@@ -91,9 +101,10 @@ variable "ingress_one" {
     description = "Inbound connection"
     from_port   = 80
     to_port     = 80
-    protocol    = "HTTP"
+    protocol    = "TCP"
     cidr_blocks = "0.0.0.0/0"
   }
+  description = "Ingress one configuration"
 }
 
 variable "ingress_two" {
@@ -102,9 +113,10 @@ variable "ingress_two" {
     description = "Inbound connection"
     from_port   = 22
     to_port     = 22
-    protocol    = "SSH"
+    protocol    = "TCP"
     cidr_blocks = "0.0.0.0/0"
   }
+  description = "Ingress two configuration"
 }
 
 variable "egress_all" {
@@ -117,19 +129,23 @@ variable "egress_all" {
     cidr_blocks      = "0.0.0.0/0"
     ipv6_cidr_blocks = "::/0"
   }
+  description = "Egress universal configuration"
 }
 
 variable "vpc_ip_range" {
   type    = string
   default = "10.10.0.0/24"
+  description = "IP range for Virtual Private Cloud"
 }
 
 variable "route_table_ip_range" {
   type    = string
   default = "0.0.0.0/0"
+  description = "IP range for Route Table"
 }
 
 variable "lb_attachment_port" {
   type    = number
   default = 80
+  description = "Load Balancer's attachment port"
 }
