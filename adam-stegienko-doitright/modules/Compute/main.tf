fa64aa49965b7cd13198e4894ff39d50f4ddc2e4
Adam Stegienko <adam.stegienko1@gmail.com>
2022-10-07 14:32:25 +0200

    small fix to below commit

--- adam-stegienko-doitright/modules/Compute/main.tf
+++ adam-stegienko-doitright/modules/Compute/main.tf
@@ -10,8 +10,5 @@ data "aws_internet_gateway" "adam-igw" {
   internet_gateway_id = var.aws_internet_gateway.id
 }
   
-data "aws_subnets" "adam-terra-subs" {
-  filter {
-    values = [var.aws_vpc.id]
-  }
+data "aws_subnet" "adam-terra-sub" {
 }
